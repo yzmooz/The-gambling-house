790596c7dd34f698941ca84c59df3aad82936436
Manabreak <49814438+Manabreaker@users.noreply.github.com>
2024-06-12 01:15:46 +0300

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -21,7 +21,8 @@ def get_user_balance():
 
 
 def update_user_balance(new_balance):
-    response = requests.post(f'{MAIN_SERVER_URL}/api/update_balance', json={'balance': new_balance}, cookies=request.cookies)
+    response = requests.post(f'{MAIN_SERVER_URL}/api/update_balance',
+                             json={'balance': new_balance}, cookies=request.cookies)
     return response.status_code == 200
 
 
@@ -36,43 +37,58 @@ def index():
     if 'bet_history' not in session:
         session['bet_history'] = []
 
-    return render_template('roulette.html', balance=balance, big_wins=session['big_wins'], bet_history=session['bet_history'])
+    return render_template('roulette.html',
+                           balance=balance, big_wins=session['big_wins'], bet_history=session['bet_history'])
 
 
 @app.route('/bet', methods=['POST'])
 def place_bet():
     global previous_win_number, current_win_number
 
-    bet = request.form.get('bet')
-    amount = int(request.form.get('amount', 0))
-    balance = get_user_balance()
+    bets = request.form.get('bets')
+    if not bets:
+        return jsonify({'result': 'error', 'message': 'Отсутствуют ставки'})
 
+    bets = eval(bets)  # Преобразуем строку JSON в словарь
+
+    balance = get_user_balance()
     if balance is None:
         return jsonify({'result': 'error', 'message': 'Ошибка авторизации. Пожалуйста, войдите на основном сайте.'})
 
-    if amount > balance:
+    total_bet_amount = sum(bets.values())
+    if total_bet_amount > balance:
         return jsonify({'result': 'error', 'message': 'Недостаточно средств на балансе'})
 
-    win, win_number = check_bet(bet, amount, balance)
+    total_win = 0
+    win_number = random.randint(0, 36)
+
+    for bet, amount in bets.items():
+        win, win_num = check_bet(bet, amount)
+        total_win += win
+        balance -= amount  # Списываем ставку со счета
 
     previous_win_number = current_win_number
     current_win_number = win_number
 
+    balance += total_win  # Добавляем выигрыш к балансу
+    update_user_balance(balance)
+
     rotation_angle = calculate_angle(current_win_number)
 
-    session['bet_history'].insert(0, {'bet': bet, 'amount': amount, 'win': win})
+    session['bet_history'].insert(0, {'bets': ", ".join(bets.keys()), 'amount': total_bet_amount, 'win': total_win})
     if len(session['bet_history']) > 10:
         session['bet_history'].pop()
 
-    if win > 5000:
-        session['big_wins'].insert(0, f'Выигрыш {win}$ ({bet.capitalize()})')
+    if total_win > 5000:
+        session['big_wins'].insert(0, f'Выигрыш {total_win}$ (Ставки: {", ".join(bets.keys())})')
         if len(session['big_wins']) > 10:
             session['big_wins'].pop()
 
+    print(session['bet_history'])
     return jsonify({
         'result': 'success',
-        'win': win,
-        'balance': get_user_balance(),
+        'win': total_win,
+        'balance': balance,
         'bet_history': session['bet_history'],
         'big_wins': session['big_wins'],
         'current_win_number': current_win_number,
@@ -97,28 +113,51 @@ def calculate_angle(num2):
     return angle + 360
 
 
-def check_bet(bet, amount, balance):
+def check_bet(bet, amount):
     win_numbers = {
-        'red': [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
-        'black': [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
-        'green': [0]
+        'RED': [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+        'BLACK': [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
+        'GREEN': [0]
     }
+
     win_number = random.randint(0, 36)
     if bet in win_numbers and win_number in win_numbers[bet]:
-        multiplier = 2 if bet in ['red', 'black'] else 35
+        multiplier = 2 if bet in ['RED', 'BLACK'] else 35
         win_amount = multiplier * amount
-        new_balance = balance + win_amount - amount
-        update_user_balance(new_balance)
         return win_amount, win_number
     elif bet.isdigit() and int(bet) == win_number:
         multiplier = 35
         win_amount = multiplier * amount
-        new_balance = balance + win_amount - amount
-        update_user_balance(new_balance)
+        return win_amount, win_number
+    elif bet == "1st 12" and 1 <= win_number <= 12:
+        multiplier = 3
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "2nd 12" and 13 <= win_number <= 24:
+        multiplier = 3
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "3rd 12" and 25 <= win_number <= 36:
+        multiplier = 3
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "1-18" and 1 <= win_number <= 18:
+        multiplier = 2
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "19-36" and 19 <= win_number <= 36:
+        multiplier = 2
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "EVEN" and win_number % 2 == 0 and win_number != 0:
+        multiplier = 2
+        win_amount = multiplier * amount
+        return win_amount, win_number
+    elif bet == "ODD" and win_number % 2 != 0:
+        multiplier = 2
+        win_amount = multiplier * amount
         return win_amount, win_number
     else:
-        new_balance = balance - amount
-        update_user_balance(new_balance)
         return 0, win_number
 
 
